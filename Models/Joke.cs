72d37b5515148aa4fcea3cc311781c898fb31292
mayarmenessy <92930799+mayarmenessy@users.noreply.github.com>
2022-09-17 07:01:51 +0200

    Add files via upload

---
+++ Models/Joke.cs
@@ -0,0 +1,15 @@
+﻿namespace JokesWebApplication.Models
+{
+    public class Joke
+    {
+        public int id { get; set; }
+        public string jokesQuestion { get; set; }
+
+        public string jokesAnswers { get; set; }
+
+        public Joke()
+        {
+
+        }
+    }
+}
